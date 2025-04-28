66d5cdc5dbb2a53b0ceb6e67da879add6afae6df
Renatcosta <renato.costa@allcomtelecom.com>
2025-04-28 12:45:44 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,1 +1,20 @@
-def 
+Banco_dados{"nome", "idade", "dinheiro"}
+nome = input("Qual seu nome?")
+idade = int(input("Qual sua idade?")
+dinheiro = int(input("Qual a quantidade de dinheiro deseja inserir/retirar?")
+               
+
+
+print("nome:", Bancos_dados(1),"idade:", Banco_dados(2),"dinheiro atual:", Banco_dados(3))
+opcao = int(input("/nSelecione uma das opções: /n1. Inserir nome /n2. Idade /n3. Inserir/Retirar Dinheiro"))
+
+while True
+  try:
+if opcao == 1:
+  print(nome)
+if opcao == 2:
+  print(idade)
+if opcao == 3:
+  print(dinheiro)
+else 
+  
